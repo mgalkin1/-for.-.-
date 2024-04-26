c9084209e989f51f55d23141dacd85f989b38960
mgalkin1 <166807265+mgalkin1@users.noreply.github.com>
2024-04-26 12:00:08 +0300

    Add files via upload

---
+++ gospodikakslojno.py
@@ -0,0 +1,8 @@
+car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in car:
+    print('Я езжу на автомобиле марки', i)
+
+car_count = 0
+for i in car:
+    car_count += 10
+    print('машин получилось', car_count)
