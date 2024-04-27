30dfa3b133de5e27e7a04a305cc2d5ac9e41422b
mgalkin1 <166807265+mgalkin1@users.noreply.github.com>
2024-04-27 11:14:42 +0300

    Update gospodikakslojno.py

--- gospodikakslojno.py
+++ gospodikakslojno.py
@@ -1,9 +1,10 @@
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 car_count = 0
 for i in car:
-print('Я езжу на автомобиле марки', i)
-car_count += 10
-print('машин получилось', car_count)
+    print('Я езжу на автомобиле марки', i)
+    car_count += 10
+    print('машин получилось', car_count)
+
 
 
 
